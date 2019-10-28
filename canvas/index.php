00d77607e184c98b456063256cde7be07b0bf101
ShuoyuGong <2788696894@qq.com>
2019-10-28 21:24:05 +0800

    once

--- canvas/index.php
+++ canvas/index.php
@@ -8,17 +8,17 @@ $arr= $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 echo json_encode($arr);
 
 
-CREATE TABLE IF NOT EXISTS `huowu_info`(
-    `id` INT UNSIGNED AUTO_INCREMENT,
-    `name` VARCHAR(100) NOT NULL,
-    `price` int NOT NULL,
-    PRIMARY KEY ( `id` )
- )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+// CREATE TABLE IF NOT EXISTS `huowu_info`(
+//     `id` INT UNSIGNED AUTO_INCREMENT,
+//     `name` VARCHAR(100) NOT NULL,
+//     `price` int NOT NULL,
+//     PRIMARY KEY ( `id` )
+//  )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
- INSERT INTO huowu_info ( id, name,price )
-    VALUES
-    ( null,"watch",2500 ),
-    ( null,"rice",5000 ),
-    ( null,"shop",3000 ),
-    ( null,"open",2300 ),
-    ( null,"obb",1800 );
+//  INSERT INTO huowu_info ( id, name,price )
+//     VALUES
+//     ( null,"watch",2500 ),
+//     ( null,"rice",5000 ),
+//     ( null,"shop",3000 ),
+//     ( null,"open",2300 ),
+//     ( null,"obb",1800 );
